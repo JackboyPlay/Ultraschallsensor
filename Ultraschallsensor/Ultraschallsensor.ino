4d2208ee5e899c0ac84f5d58ccd92c46944322bb
Johannes Leutgeb <johannes.leutgeb2002@gmail.com>
2018-12-01 11:18:47 +0100

    Projektdateien hinzufügen.

---
+++ Ultraschallsensor/Ultraschallsensor.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Ultraschallsensor.ino
+ Created:	01.12.2018 11:17:19
+ Author:	Johannes
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
